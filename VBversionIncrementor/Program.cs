f417947d0deddf0e38b88e9138bff44c31d84cae
kozintsev <o.kozintsev@gmail.com>
2016-10-08 09:54:42 +0400

    del AssemblyFileVersion
    так как она берётся из AssemblyVersion

--- VBversionIncrementor/Program.cs
+++ VBversionIncrementor/Program.cs
@@ -1,17 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
+﻿using System.Diagnostics;
 using System.IO;
-using System.Linq;
-using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
 namespace VBversionIncrementor
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             var process = new Process
             {
@@ -27,7 +22,7 @@ namespace VBversionIncrementor
             var verFromTag = process.StandardOutput.ReadToEnd();
             verFromTag = Regex.Replace(verFromTag, "\r\n", "");
 
-            string text = File.ReadAllText(@"..\..\..\LabelPrint\My Project\AssemblyInfo.vb");
+            var text = File.ReadAllText(@"..\..\..\LabelPrint\My Project\AssemblyInfo.vb");
             text = Regex.Replace(text, @"\r\n<Assembly: AssemblyVersion\((.*?)\)", "\r\n<Assembly: AssemblyVersion(\"" + verFromTag + "\")");
             File.WriteAllText(@"..\..\..\LabelPrint\My Project\AssemblyInfo.vb", text);
         }
